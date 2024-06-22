3bb77fe9fbe1bee6e08876475a01308b08dbec97
Vishrut <165580939+Vishrut7080@users.noreply.github.com>
2024-06-22 15:49:58 +0530

    Randomize Food

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const playBoard = document.querySelector(".play-area");
 
 let foodX = 15, foodY = 20;
 
+const changeFoodPoa = () => {
+    //Passing a random 0 - 30 value as food position
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+    console.log(foodX, foodY);
+}
+
 const initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodX}/${foodY}"></div>`;
     playBoard.innerHTML = htmlMarkup;
 }
+changeFoodPoa();
 initGame();
