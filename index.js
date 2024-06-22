65249c6fa40676366881d2a365cd7e8cfb2dcd48
Vishrut <165580939+Vishrut7080@users.noreply.github.com>
2024-06-22 16:15:08 +0530

    Make head move after pressing arrow keys

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const playBoard = document.querySelector(".play-area");
 
 let foodX, foodY;
 let snakeX = 5, snakeY = 10;
+let velocityX = 0, velocityY = 0;
 
 const changeFoodPoa = () => {
     //Passing a random 0 - 30 value as food position
@@ -10,10 +11,33 @@ const changeFoodPoa = () => {
     console.log(foodX, foodY);
 }
 
+const changeDirection = (e) => {
+    // console.log(e)
+    //Changing velocity values based on key press
+    if (e.key === "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.key === "ArrowDown") {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.key === "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    } else if (e.key === "ArrowLeft") {
+        velocityX = -1;
+        velocityY = 0;
+    }
+}
+
 const initGame = () => {
-    let htmlMarkup = `<div class="food" style="grid-area: ${foodX}/${foodY}"></div>`;
-    htmlMarkup += `<div class="snake" style="grid-area: ${snakeX}/${snakeY}"></div>`;
+    let htmlMarkup = `<div class="food" style="grid-area: ${foodY}/${foodX}"></div>`;
+    //Updating snake head's position based on current velocity
+    snakeX += velocityX;
+    snakeY += velocityY;
+    htmlMarkup += `<div class="snake" style="grid-area: ${snakeY}/${snakeX}"></div>`;
     playBoard.innerHTML = htmlMarkup;
 }
 changeFoodPoa();
-initGame();
+setInterval(initGame, 125)
+
+document.addEventListener("keydown", changeDirection);
